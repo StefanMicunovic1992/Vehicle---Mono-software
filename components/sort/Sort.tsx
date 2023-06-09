fc091897d3466b5b77e2994f489f8c7096e6b71b
stefanmicunovic <stefanmicunovic1992@gmail.com>
2023-06-09 11:24:06 +0200

    set up sort component

---
+++ components/sort/Sort.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import React from 'react';
+import { useVehiclesStore } from '@/store/store';
+import { observer } from 'mobx-react';
+import Select from 'react-select';
+
+const optionsForSort = [
+  { value: 'random', label: 'random' },
+  { value: 'price (desc)', label: 'price (desc)' },
+  { value: 'price (asc)', label: 'price (asc)' },
+  { value: 'name (desc)', label: 'name (decs)' },
+  { value: 'name (asc)', label: 'name (asc)' },
+];
+
+interface OptionsForSortProps {
+  value: string;
+  label: string;
+}
+
+function Sort() {
+  const vehiclesStore = useVehiclesStore();
+  const sortByPrice = (selected: OptionsForSortProps | null) => {
+    const inputString = selected?.value;
+
+    if (
+      inputString !== null && inputString !== 'random' && inputString !== undefined) {
+      const parts = inputString.split('(');
+      const typeOfSort = parts[0].trim();
+      const descOrAsc = parts[1].slice(0, -1).trim();
+
+      if (typeOfSort === 'price') {
+        vehiclesStore.sortByPrice(descOrAsc);
+      } else {
+        vehiclesStore.sortByName(descOrAsc);
+      }
+    } else {
+      vehiclesStore.sortRandom();
+    }
+  };
+  return (
+    <article>
+      <h3>Sort by...</h3>
+      <Select placeholder="sort..." onChange={sortByPrice} options={optionsForSort} />
+    </article>
+  );
+}
+
+export default observer(Sort);
