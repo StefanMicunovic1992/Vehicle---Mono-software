935bb18b5235622e478f5f98d0ef9453b1f25256
stefanmicunovic <stefanmicunovic1992@gmail.com>
2023-06-09 20:07:37 +0200

    new design of Vehicle component

--- components/vehicle/Vehicle.tsx
+++ components/vehicle/Vehicle.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import './Vehicle.scss';
+import React, { useState } from 'react';
 import VehicleI from '@/interface/Vehicle';
 import { observer } from 'mobx-react';
 import { useVehiclesStore } from '@/store/store';
-import React, { useState } from 'react';
-import Image from 'next/image';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import { FaMoneyBillAlt } from 'react-icons/fa';
+import DeleteIcon from '@mui/icons-material/Delete';
+import PriceChangeOutlinedIcon from '@mui/icons-material/PriceChangeOutlined';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 import SetNewPrice from '../set-new-price/SetNewPrice';
 
 interface VehicleProps {
@@ -15,7 +21,9 @@ interface VehicleProps {
 
 function Vehicle({ car, setShowOrHideGallery }: VehicleProps) {
   const vehiclesStore = useVehiclesStore();
+
   const [showOrHideNewPrice, setShowOrHideNewPrice] = useState<boolean>(false);
+
   const {
     name,
     model,
@@ -34,19 +42,27 @@ function Vehicle({ car, setShowOrHideGallery }: VehicleProps) {
   };
 
   return (
-    <article className="car">
-      <h2>{[name, ' - ', model]}</h2>
-      <h3>{price}</h3>
+    <Card className="car">
+      <h2>{[name, ' ', model]}</h2>
+      <CardMedia
+        className="imageOfCar"
+        component="img"
+        image={image[0]}
+        alt={model}
+        onClick={showCarusel}
+      />
+      <div className="price">
+        <FaMoneyBillAlt className="icon" />
+        <h3>{price}</h3>
+      </div>
       {showOrHideNewPrice && <SetNewPrice id={id} setShowOrHideNewPrice={setShowOrHideNewPrice} />}
-      <Image src={image[0]} className="imageOfCar" width={1000} height={1000} alt={model} onClick={showCarusel} />
-      <h3>{id}</h3>
-      <button type="button" onClick={deleteVehicle}>
-        DELETE
-      </button>
-      <button type="button" onClick={() => setShowOrHideNewPrice(true)}>
-        UPDATA
-      </button>
-    </article>
+      <div>
+        <Stack direction="row" spacing={3}>
+          <Button variant="contained" size="small" startIcon={<PriceChangeOutlinedIcon />} onClick={() => setShowOrHideNewPrice(true)}>Change price</Button>
+          <Button variant="contained" size="small" color="error" startIcon={<DeleteIcon />} onClick={deleteVehicle}>Delete vehicle</Button>
+        </Stack>
+      </div>
+    </Card>
   );
 }
 
