918d5f9023f0e1f65e9769cdd74444e43fb529d8
stefanmicunovic <stefanmicunovic1992@gmail.com>
2023-06-12 21:21:50 +0200

    new set for image domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['imgd.aeplcdn.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 };
 
